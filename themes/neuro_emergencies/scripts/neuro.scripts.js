493a28b84e0a0dcd5680b6e19fa6dfd55184f013
vatche-chamlian <vatche.chamlian@penzias.com>
2015-06-12 14:52:18 +0000

    theme changes for collapsible bios on faculty sidebar

--- themes/neuro_emergencies/scripts/neuro.scripts.js
+++ themes/neuro_emergencies/scripts/neuro.scripts.js
@@ -1,8 +1,8 @@
 (function ($) {
   Drupal.behaviors.generalScripts = {
     attach: function (context, settings) {
-      if($('body').hasClass('page-faculty') || $('body').hassClass('page-3');) {
-	$('collapse-me-control').click(function(){
+      if($('body').hasClass('page-faculty')) {
+	$('.collapse-me-control').click(function(){
 	   $(this).toggleClass('open').prev().slideToggle();
         });
       }	
